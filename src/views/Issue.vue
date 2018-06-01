0496ac1f4003a71eef9d73609f15ac7ad24b4bd5
Marc-Antoine Ferland <maf.ferland@gmail.com>
2018-06-01 01:28:55 -0400

    add missing issue components binding

--- src/views/Issue.vue
+++ src/views/Issue.vue
@@ -1,12 +1,17 @@
 <template>
   <div class='container'>
+    <search-issue/>
+    <list-issue/>
   </div>
 </template>
 
 <script>
+import SearchIssue from '@/components/issue/SearchIssue';
+import ListIssue from '@/components/issue/ListIssue';
 
 export default {
   name: 'Issue',
+  components: { SearchIssue, ListIssue }
 };
 </script>
 
